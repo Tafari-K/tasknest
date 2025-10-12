ebc143a9b54da1431c918651d98f0827a2605dd9
Tafari-K <kirton.tafari@gmail.com>
2025-10-12 17:18:22 +0100

    Create: add html files, update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('jobs/', views.jobs, name='jobs'),
+    path('search/', views.search_jobs, name='search_jobs'),
+    path('delete-job/', views.delete_job, name='delete_job'),
 ]
